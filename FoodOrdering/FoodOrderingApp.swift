5732a40613052b074d150d7d1cc395d7031f2ed1
Vy Tran <vlt002@bucknell.edu>
2022-11-19 14:58:07 -0500

    Initial Commit

---
+++ FoodOrdering/FoodOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingApp.swift
+//  FoodOrdering
+//
+//  Created by Vy Tran on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
